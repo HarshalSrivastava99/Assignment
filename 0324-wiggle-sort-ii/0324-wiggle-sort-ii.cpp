03a79044c2bcd465c8b5ae72c4eef07ef03b1a93
HarshalSrivastava99 <harshalsrivastava100@gmail.com>
2026-08-31 19:07:04 +0530

    Time: 5 ms (17.6%), Space: 22 MB (47.16%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        vector<int>temp=nums;
-        sort(temp.begin(),temp.end());
+        // question m yeh krna h ki pehle smaller fir greater fir smaller fir greater and so on
+        vector<int>temp=nums;//temp ek new array h
+        sort(temp.begin(),temp.end());//temp m sorted array hoga 
         int n=nums.size();
-        int mid=(n-1)/2;
+        int mid=(n-1)/2;//yeh array ko 2 half m divide kr dega
         int high=n-1;
         for(int i=0;i<n;i++)
         {
         if(i%2 == 0)
         {
-        nums[i]=temp[mid--];
+        nums[i]=temp[mid--];//pehle yeh aayega fir else wala fir ye fir else jb tk khtm nhi hota h array
         }
         else
         {
